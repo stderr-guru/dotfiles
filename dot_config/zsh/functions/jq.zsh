22c86d194eebdbae09a723efb626c26ef848fe89
Jordan Lowell <jordan@stderr.guru>
2026-08-05 10:53:27 -0400

    Enhance jqschema function and clean up aliases
    Updated jqschema function to use colored output with jq -C and removed jqischema alias.

--- dot_config/zsh/functions/jq.zsh
+++ dot_config/zsh/functions/jq.zsh
@@ -1,13 +1,8 @@
 jqschema() {
   local jq_filter='walk(if type == "array" then map(type) | unique elif type == "object" then . else type end)'
-  jq "$jq_filter" "$@"
+  jq -C "$jq_filter" "$@"
 }
 
 alias jqs="jqschema"
-
-jqischema() {
-  jnv --default-filter 'walk(if type == "array" then map(type) | unique elif type == "object" then . else type end)'
-}
-
 alias jqi="jnv"
-alias jqis="jqischema"
+
