f59318feaa89dbee701149d1c9e5f7d39efb7f23
Jordan Lowell <jordan@stderr.guru>
2026-08-05 10:43:53 -0400

    Fix jqischema function definition

--- dot_config/zsh/functions/jq.zsh
+++ dot_config/zsh/functions/jq.zsh
@@ -6,7 +6,7 @@ jqschema() {
 alias jqs="jqschema"
 
 jqischema() {
-  jnv --default-filter 'walk(if type == "array" then map(type) | unique elif type == "object" then . else type end)' "
+  jnv --default-filter 'walk(if type == "array" then map(type) | unique elif type == "object" then . else type end)'
 }
 
 alias jqi="jnv"
